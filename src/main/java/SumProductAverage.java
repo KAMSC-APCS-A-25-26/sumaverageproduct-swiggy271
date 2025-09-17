e1957b1f3c294645f110a7b7a6c3a7a26e8a1f89
shuler wiegerink <stellarpie225@gmail.com>
2025-09-17 11:04:00 -0400

    Update SumProductAverage.java

--- src/main/java/SumProductAverage.java
+++ src/main/java/SumProductAverage.java
@@ -15,8 +15,36 @@
 
 import java.util.Scanner;
 
-public class SumProductAverage {
+public class SumProductAverage
+{
     public static void main(String[] args) {
-        // TODO
+        Scanner sc = new Scanner(System.in);
+
+        // signature
+        System.out.println("Shuler Wiegerink\nAPCS-A\n2.2-2.3 program");
+        // prompt user for the number of runs
+        System.out.println("\n\nHow many timed would you like the program to run:  ");
+        int numRuns = sc.nextInt();
+
+        for (int i = 0; i < numRuns; i++) {
+            double num1, num2, num3, sum;
+
+            // prompt user for the inputs
+            System.out.println("\nPlease enter 3 numbers");
+            num1 = sc.nextDouble();
+            num2 = sc.nextDouble();
+            num3 = sc.nextDouble();
+
+            // output the sum
+            System.out.println("\nThe sum of these numbers is " + (num1 + num2 + num3));
+
+            // output the product
+            System.out.println("The product of these numbers is " + (num1 * num2 * num3));
+
+            // output the average
+            System.out.println("The average of these numbers is " + ((num1 + num2 + num3) / 3) + "\n\n");
+        }
     }
 }
+
+
